25b136295d9eca53a8f4c3982866d83c95044a3e
ErikWe <erikwestberg7@gmail.com>
2024-08-03 03:43:23 +0200

    Initial implementation

---
+++ src/Models/ICSharpTypeArgumentData.cs
@@ -0,0 +1,10 @@
+﻿namespace Paraminter.Arguments.CSharp.Type.Models;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+/// <summary>Represents syntactic data about a C# type argument.</summary>
+public interface ICSharpTypeArgumentData
+{
+    /// <summary>The syntactic data about the C# type argument.</summary>
+    public abstract TypeSyntax Argument { get; }
+}
